6bc768becd92c8a2ccb18c1e0f832d1bd700f687
Kilowa Devs <kilowadevs@gmail.com>
2026-09-06 07:20:04 -0300

    asd

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ http
   .createServer((req, res) => {
     if (req.method === 'GET' && (req.url === '/' || req.url === '/health')) {
       res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-      res.end('hola diply\n');
+      res.end('hola diplyy\n');
       return;
     }
     res.writeHead(404);
