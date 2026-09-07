5427abfb48ea914e63021863d02d10f736f894c4
Kilowa Devs <kilowadevs@gmail.com>
2026-09-07 18:11:03 -0300

    asd

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ const port = Number(process.env.PORT ?? 3000);
 http
   .createServer((req, res) => {
     console.log(`[hello-http] ${req.method} ${req.url}`);
-    console.log("hola leio");
+    console.log("hola leioa");
     if (req.method === 'GET' && (req.url === '/' || req.url === '/health')) {
       res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
       res.end('hola diply\n');
